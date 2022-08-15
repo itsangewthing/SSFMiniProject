f7d4ef14262753c15a38956665b821581a290d8f
itsangewthing <yann.ngew@gmail.com>
2022-08-15 18:50:22 +0800

    2nd msg

---
+++ src/main/java/vttp2022/NUSISS/SSF/Mini/Project/SsfMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.NUSISS.SSF.Mini.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfMiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfMiniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/NUSISS/SSF/Mini/Project/SsfMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.NUSISS.SSF.Mini.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
